05f70f583e6de301794685ba82db3234c189c63e
Jake <jakeroggenbuck2@gmail.com>
2022-09-29 14:11:57 -0700

    Add get callback
    - p.s. wow, that was kinda hard. I really had very little documentation
      to go off of. I got it working pretty fast though.

--- src/main.c
+++ src/main.c
@@ -3,13 +3,31 @@
 #include <stdio.h>
 #include <sys/stat.h>
 
+struct PR {
+    char name[100];
+    char exercise[100];
+    double weight;
+    int reps;
+};
+
 bool file_exists(char *filename) {
     struct stat buffer;
     return (stat(filename, &buffer) == 0);
 }
 
-static int callback(void *not_used, int argc, char **argv, char **az_col_name) {
-    for (int i = 0; i < argc; i++) {
+static int get_callback(void *data, int argc, char **argv, char **az_col_name) {
+    struct PR *prs = (struct PR *)data;
+    struct PR new = {"Jake", "Squat", 245, 1};
+    prs[0] = new;
+
+    return 0;
+}
+
+static int callback(void *data, int argc, char **argv, char **az_col_name) {
+    int i;
+    fprintf(stderr, "%s: ", (const char *)data);
+
+    for (i = 0; i < argc; i++) {
         printf("%s = %s\n", az_col_name[i], argv[i] ? argv[i] : "NULL");
     }
 
@@ -28,10 +46,16 @@ int create_database_table(sqlite3 *db) {
                 "REPS        	INT		NOT NULL);";
 
     rc = sqlite3_exec(db, sql, callback, 0, &error_message);
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "SQL error: %s\n", error_message);
+        sqlite3_free(error_message);
+    } else {
+        fprintf(stdout, "Operation done successfully.\n");
+    }
     return !rc;
 }
 
-int add(char *name, char *exercise, double weight, int reps, sqlite3 *db) {
+int add(sqlite3 *db, char *name, char *exercise, double weight, int reps) {
     int rc;
     char *error_message;
     char sql[300];
@@ -42,6 +66,30 @@ int add(char *name, char *exercise, double weight, int reps, sqlite3 *db) {
             name, exercise, weight, reps);
 
     rc = sqlite3_exec(db, sql, callback, 0, &error_message);
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "SQL error: %s\n", error_message);
+        sqlite3_free(error_message);
+    } else {
+        fprintf(stdout, "Operation done successfully.\n");
+    }
+    return !rc;
+}
+
+int get(sqlite3 *db, struct PR *prs, char *name) {
+    int rc;
+    char *error_message;
+    char sql[300];
+
+    sprintf(sql, "SELECT * FROM PRS WHERE NAME='%s'", name);
+
+    rc = sqlite3_exec(db, sql, get_callback, (void *)prs, &error_message);
+
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "SQL error: %s\n", error_message);
+        sqlite3_free(error_message);
+    } else {
+        fprintf(stdout, "Operation done successfully.\n");
+    }
     return !rc;
 }
 
@@ -65,11 +113,15 @@ int main() {
             printf("Could not create table.\n");
             return 1;
         }
+
+        if (!add(db, "Jake", "Bench", 135, 10)) {
+            printf("Error adding to database.\n");
+        }
     }
 
-    if (!add("Jake", "Bench", 135, 10, db)) {
-        printf("Error adding to database.\n");
-    }
+    struct PR pr[10];
+    get(db, pr, "Jake");
+    printf("%s", pr[0].exercise);
 
     sqlite3_close(db);
 
